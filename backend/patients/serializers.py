25ead9ef7a6a979c74d4bb25cbb1ef448576c6a3
dikshakookie <cherryg11122005@gmail.com>
2026-07-10 02:52:59 +0530

    Build doctors app views/urls; restore patients serializer

--- backend/patients/serializers.py
+++ backend/patients/serializers.py
@@ -1,3 +1,18 @@
 from rest_framework import serializers
+from .models import Patient
 
-# Define DRF serializers here.
+
+class PatientSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Patient
+        fields = [
+            "id", "patient_id", "full_name", "dob", "gender",
+            "blood_group", "address", "insurance", "allergies",
+            "emergency_contact", "past_diseases",
+        ]
+        read_only_fields = ["id", "patient_id"]
+
+    def get_full_name(self, obj):
+        return obj.user.get_full_name() or obj.user.username
